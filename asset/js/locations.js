b7901f35dfcd4aa4128d2af542c4a0d007805a9f
Constant Pagoui <constantpagoui@gmail.com>
2017-10-23 21:43:45 -0400

    Create locations.js
    Adds data for locations
    includes: title, position, and type of the location
    provides markers with object point.

---
+++ asset/js/locations.js
@@ -0,0 +1,84 @@
+
+// These are the locations listings that will be shown to the user.
+var neigborhood_locations = [
+    {
+        title: 'Bentley Road South East',
+        position: { lat: 33.9177763, lng:  -84.47558609999999 },
+        type: 'Ghetto neighborhood'
+    },
+    {
+        title: 'Cosmopolitan Live',
+        position: { lat: 33.9231882, lng: -84.47979409999999 },
+        type: 'Bar & Restaurant'
+    },
+    {
+        title: 'Reliable',
+        position:{ lat: 33.923889, lng: -84.47273469999999 },
+        type:  'Limousine service and Transit'
+    },
+	{
+        title: 'Minks Package',
+        position:{ lat: 33.9226044, lng:-84.4770456 },
+        type:  'Liquor store'
+    },
+    {
+        title: 'Rainbow Shops',
+        position: { lat: 33.9229048, lng: -84.46666499999998 },
+        type:  'Shopping'
+    },
+    {
+        title: 'Bowlero Marietta',
+        position:{ lat: 33.92401280000001, lng: -84.4739472 },
+        type: 'Entertainment'
+    },
+    {
+        title: 'Stratford Ridge',
+        position: { lat: 33.9217291, lng: -84.47867559999997 },
+        type: 'Appartment Building'
+    },
+    {
+        title: 'Church of Christ',
+        position: { lat: 33.9314198, lng: -84.47572600000001 },
+        type: 'Church'
+    },
+    {
+        title: 'Dollar General',
+        position: { lat: 33.928505, lng: -84.493155 },
+        type: 'Home Goods Store'
+    },
+    {
+        title: 'Rio and Lounge',
+        position: { lat: 33.9393016, lng: -84.50614769999999 },
+        type: 'Club'
+    },
+    {
+        title: 'Pappasito\'s Cantina',
+        position: { lat: 33.90198880000001, lng: -84.47106329999997 },
+        type: 'Restaurant'
+    },
+    {
+        title: 'Kroger',
+        position: {lat: 33.9230133, lng: -84.46993789999999 },
+        type: 'Grocery Store'
+    },
+	{
+        title: 'Kennesaw State University',
+        position: {lat: 33.9390885, lng: -84.51916260000002 },
+        type: 'School'
+    },
+	{
+        title: 'Covered Bridge Condonium',
+        position: {lat: 33.9295541, lng: -84.48087220000002 },
+        type: 'Complex'
+    },
+	{
+        title: 'Cracker Barrel Old Country Store',
+        position: {lat: 33.9218911, lng: -84.49152989999999 },
+        type: 'Restaurant'
+    },
+	{
+        title: 'Dave & Buster\'s',
+        position: {lat: 33.919681, lng: -84.4852 },
+        type: 'Sport Bar'
+    }
+];
